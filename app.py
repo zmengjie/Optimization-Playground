67ec8ec72b1c67cbd6cb73a8c043bd6fec0af4c4
Mengjie <e1285298@u.nus.edu>
2025-08-07 14:36:17 +0800

    style

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 import streamlit as st
 
+st.markdown(
+    """
+    <style>
+    /* Make the content container full-width */
+    .block-container {
+        padding-left: 2rem !important;
+        padding-right: 2rem !important;
+        max-width: 100% !important;
+    }
+
+    /* Optional: Make the page background wider as well */
+    .main {
+        max-width: 100vw !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
 st.set_page_config(page_title="Optimization Playground", layout="wide")
 
 
