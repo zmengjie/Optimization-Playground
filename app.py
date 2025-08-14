64732312faaed7763343ee97262be22b41f1e2b2
Mengjie <e1285298@u.nus.edu>
2025-08-14 12:37:15 +0800

    fix

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-from streamlit_analytics import TrackPage
-
-
 
 st.markdown(
     """
@@ -26,24 +23,24 @@ st.markdown(
 
 st.set_page_config(page_title="Optimization Playground", layout="wide")
 
-with TrackPage():
-    # Title and main description
-    st.title("🎯 Welcome to the Optimization Playground")
 
-    st.markdown("""
-    This interactive app lets you explore:
+# Title and main description
+st.title("🎯 Welcome to the Optimization Playground")
 
-    - 📘 **Key optimization concepts** like gradient, Hessian, KKT conditions  
-    - 🧪 **Playground** for experimenting with optimizers on custom functions  
-    - 📐 **Symbolic tools** for visualizing Taylor expansions, curvature, and more  
-    - 🧮 **Taylor Series** module to understand how optimizers arise from first- and second-order approximations
+st.markdown("""
+This interactive app lets you explore:
 
-    """)
+- 📘 **Key optimization concepts** like gradient, Hessian, KKT conditions  
+- 🧪 **Playground** for experimenting with optimizers on custom functions  
+- 📐 **Symbolic tools** for visualizing Taylor expansions, curvature, and more  
+- 🧮 **Taylor Series** module to understand how optimizers arise from first- and second-order approximations
 
-    st.info("👉 Use the sidebar to dive into each section. You can switch between **Taylor Series** and **Optimizer Playground** tabs.")
+""")
 
-    st.markdown(
-        "If you have any feedback regarding the application, kindly fill out this [form](https://forms.gle/tae4s9EH5dqcG6rm9).",
-        unsafe_allow_html=True
-    )
+st.info("👉 Use the sidebar to dive into each section. You can switch between **Taylor Series** and **Optimizer Playground** tabs.")
+
+st.markdown(
+    "If you have any feedback regarding the application, kindly fill out this [form](https://forms.gle/tae4s9EH5dqcG6rm9).",
+    unsafe_allow_html=True
+)
 
